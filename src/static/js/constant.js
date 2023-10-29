6562cc170c706f17f5da6556417926cc0ff09844
Your Name <you@example.com>
2023-10-29 23:06:39 +0800

    first commit

---
+++ src/static/js/constant.js
@@ -0,0 +1,15 @@
+const CONSTANT = {
+    UNASSIGNED: 0,
+    GRID_SIZE: 9,
+    BOX_SIZE: 3,
+    NUMBERS: [1,2,3,4,5,6,7,8,9],
+    LEVEL_NAME: [
+        '简单',
+        '中等',
+        '困难',
+        '非常难',
+        '地狱',
+        '炼狱'
+    ],
+    LEVEL: [29, 38, 47, 56, 65, 74]
+}
